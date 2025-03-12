c130b4bc884a8c329affc050285f1db56fc89454
bruefire <sprinkla.sendonly@gmail.com>
2025-03-13 04:37:34 +0900

    updated logic to deploy javascript file.

--- __init__.py
+++ __init__.py
@@ -12,9 +12,11 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "VFrame Loader With Mask Editor" : "SequentialImageLoader",
     "Video Loader With Mask Editor" : "VideoFrameExtractor",
 }
-__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+WEB_DIRECTORY = "./js"
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS', "WEB_DIRECTORY"]
 
 
+# keep this for backward compatibility for a while..
 module_js_directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), "js")
 application_root_directory = os.path.dirname(folder_paths.__file__)
 application_web_extensions_directory = os.path.join(application_root_directory, "web", "extensions", "SequentialImageLoader")
